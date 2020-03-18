c33264fe92d22590b20f02392a0980b1faacd826
TurtlSandwich <i398785@fhict.nl>
2020-03-18 12:39:18 +0100

    Initial commit

---
+++ src/main/java/com/FunPersoonlijkProject/Backend/FunBackend/FunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.FunPersoonlijkProject.Backend.FunBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FunPersoonlijkProject/Backend/FunBackend/FunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FunPersoonlijkProject.Backend.FunBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
